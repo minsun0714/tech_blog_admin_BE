d678fd6888e05323b4f71114169d968add19f6c6
이민선 <mslee@anchors-biz.com>
2026-06-28 20:10:31 +0900

    feat: 태그 command service 생성

---
+++ src/main/java/com/blog/be/tag/application/TagCommandService.java
@@ -0,0 +1,59 @@
+package com.blog.be.tag.application;
+
+import com.blog.be.tag.domain.TagErrorCode;
+import com.blog.be.tag.domain.TagException;
+import com.blog.be.tag.domain.TagRepository;
+import com.blog.be.tag.infrastructure.persistence.TagJpaEntity;
+import jakarta.transaction.Transactional;
+import lombok.RequiredArgsConstructor;
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.stereotype.Service;
+
+import java.util.Collection;
+import java.util.Set;
+import java.util.stream.Collectors;
+
+@Slf4j
+@Service
+@Transactional
+@RequiredArgsConstructor
+public class TagCommandService {
+
+    private final TagRepository tagRepository;
+
+    public void upsertTag(String name) {
+        if (tagRepository.existsByName(name)) return;
+
+        TagJpaEntity tag = TagJpaEntity.create(name);
+
+        tagRepository.save(tag);
+    }
+
+    public Set<Long> upsertAllAndGetIds(Collection<String> tagNames) {
+        Set<TagJpaEntity> tagJpaEntities = tagNames.stream()
+                .map(TagJpaEntity::create)
+                .collect(Collectors.toSet());
+
+        return tagRepository.saveAll(tagJpaEntities)
+                .stream()
+                .map(TagJpaEntity::getId)
+                .collect(Collectors.toSet());
+    }
+
+    public void updateTagName(Long id, String newName) {
+        TagJpaEntity tag = getTag(id);
+
+        tag.changeName(newName);
+    }
+
+    public void deleteTag(Long id){
+        TagJpaEntity tag = getTag(id);
+
+        tagRepository.delete(tag);
+    }
+
+    private TagJpaEntity getTag(Long id) {
+        return tagRepository.findById(id)
+                .orElseThrow(() -> new TagException(TagErrorCode.TAG_NOT_FOUND));
+    }
+}
