e82904c54d03b570f6c813955529342127b6e27a
이민선 <mslee@anchors-biz.com>
2026-06-28 20:10:24 +0900

    feat: 시리즈 command service 생성

---
+++ src/main/java/com/blog/be/series/application/SeriesCommandService.java
@@ -0,0 +1,41 @@
+package com.blog.be.series.application;
+
+import com.blog.be.series.domain.SeriesErrorCode;
+import com.blog.be.series.domain.SeriesException;
+import com.blog.be.series.domain.SeriesRepository;
+import com.blog.be.series.infrastructure.persistence.SeriesJpaEntity;
+import jakarta.transaction.Transactional;
+import lombok.RequiredArgsConstructor;
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.stereotype.Service;
+
+@Slf4j
+@Service
+@Transactional
+@RequiredArgsConstructor
+public class SeriesCommandService {
+
+    private final SeriesRepository seriesRepository;
+
+    public void createSeries(String name) {
+        SeriesJpaEntity series = SeriesJpaEntity.create(name);
+        seriesRepository.save(series);
+    }
+
+    public void updateSeriesName(Long id, String newName) {
+        SeriesJpaEntity series = getSeries(id);
+
+        series.changeName(newName);
+    }
+
+    public void deleteSeries(Long id){
+        SeriesJpaEntity series = getSeries(id);
+
+        seriesRepository.delete(series);
+    }
+
+    private SeriesJpaEntity getSeries(Long id) {
+        return seriesRepository.findById(id)
+                .orElseThrow(() -> new SeriesException(SeriesErrorCode.SERIES_NOT_FOUND));
+    }
+}
