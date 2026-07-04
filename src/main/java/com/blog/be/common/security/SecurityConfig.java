0c960db7484eb728021f23c4a431a56b2f6301c5
이민선 <mslee@anchors-biz.com>
2026-07-04 10:40:01 +0900

    fix: error 로그 볼 수 있도록 SecurityConfig 수정

--- src/main/java/com/blog/be/common/security/SecurityConfig.java
+++ src/main/java/com/blog/be/common/security/SecurityConfig.java
@@ -26,6 +26,7 @@ public class SecurityConfig {
                 .sessionManagement(session ->
                         session.sessionCreationPolicy(SessionCreationPolicy.STATELESS))
                 .authorizeHttpRequests(auth -> auth
+                        .requestMatchers("/error").permitAll()
                         .requestMatchers(HttpMethod.GET, "/**").permitAll()
                         .anyRequest().authenticated())
                 .addFilterBefore(new ApiKeyFilter(apiKey), UsernamePasswordAuthenticationFilter.class);
