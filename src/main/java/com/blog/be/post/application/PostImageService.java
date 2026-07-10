be6d216af985bd1c45b043eb94777f2893337416
이민선 <mslee@anchors-biz.com>
2026-07-10 21:00:08 +0900

    feat: 이미지 S3 업로드부터 다시 개발

--- src/main/java/com/blog/be/post/application/PostImageService.java
+++ src/main/java/com/blog/be/post/application/PostImageService.java
@@ -4,6 +4,7 @@ import java.util.List;
 
 import com.blog.be.post.application.dto.DeletedPostImage;
 import com.blog.be.post.infrastructure.persistence.PostImageJpaEntity;
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.stereotype.Service;
 
 import com.blog.be.post.domain.PostImageRepository;
@@ -11,6 +12,7 @@ import com.blog.be.post.domain.PostImageRepository;
 import jakarta.transaction.Transactional;
 import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
+import org.springframework.web.multipart.MultipartFile;
 
 @Slf4j
 @Service
@@ -18,37 +20,13 @@ import lombok.extern.slf4j.Slf4j;
 @RequiredArgsConstructor
 public class PostImageService {
 
-	private final PostImageRepository postImageRepository;
+	@Value("{cloud.aws.s3.base-url")
+	private String s3BaseUrl;
 
-	public void restore(Long postId, boolean isThumbnail, String s3Key) {
-		PostImageJpaEntity postImageJpaEntity = PostImageJpaEntity.create(postId, s3Key, isThumbnail);
-		postImageRepository.restore(postImageJpaEntity);
-	}
+	private final ImageStorage imageStorage;
 
-	public void restoreAll(List<DeletedPostImage> deletedPostImageList) {
-		List<PostImageJpaEntity> postImageJpaEntities = deletedPostImageList.stream()
-						.map(deletedPostImage -> PostImageJpaEntity.create(deletedPostImage.postId(), deletedPostImage.s3Key(), deletedPostImage.isThumbnail()))
-						.toList();
-
-		postImageRepository.restoreAll(postImageJpaEntities);
-	}
-
-	public String uploadPostImage(Long postId, String s3Key, boolean isThumbnail) {
-		return postImageRepository.saveFile(postId, s3Key, isThumbnail);
-	}
-
-	public List<DeletedPostImage> deletePostImagesByPostId(Long postId) {
-		List<PostImageJpaEntity> postImageJpaEntities = postImageRepository.findAllByPostId(postId);
-		postImageRepository.deleteAllByPostId(postId);
-		return postImageJpaEntities.stream()
-				.map(postImageJpaEntity ->
-						DeletedPostImage.of(postImageJpaEntity.getPostId(), postImageJpaEntity.isThumbnail(), postImageJpaEntity.getS3Key()))
-				.toList();
-	}
-
-	public DeletedPostImage deletePostImage(Long imageId) {
-		PostImageJpaEntity postImageJpaEntity = postImageRepository.findById(imageId);
-		postImageRepository.deleteById(imageId);
-		return DeletedPostImage.of(postImageJpaEntity.getPostId(), postImageJpaEntity.isThumbnail(), postImageJpaEntity.getS3Key());
+	public String uploadAndGetImageUrl(MultipartFile file) {
+		String s3Key = imageStorage.upload(file);
+		return String.join("/", s3BaseUrl, s3Key);
 	}
 }
